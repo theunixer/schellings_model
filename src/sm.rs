8c06454029ab3c8403bf8af2be7f0a811a6518bf
GreatC0der <none@127.0.0.1>
2023-04-28 19:22:51 +0100

    fn moved to separate module and refactored

---
+++ src/sm.rs
@@ -0,0 +1,107 @@
+pub mod sm {
+    use rand::prelude::RngCore;
+    use rand::thread_rng;
+    use std::mem::transmute;
+    #[derive(PartialEq, Clone, Copy)]
+    pub enum Colour {
+        Yellow,
+        Green,
+    }
+
+    #[derive(PartialEq, Clone, Copy)]
+    pub struct Agent {
+        pub colour: Colour,
+        pub happiness: f32,
+    }
+
+    pub struct Field {
+        pub field: Vec<Vec<Option<Agent>>>,
+    }
+
+    impl Field {
+        pub fn new(x: usize, y: usize) -> Self {
+            let mut f: Vec<Vec<Option<Agent>>> = Vec::new();
+
+            for xx in 0..x {
+                let mut temp_vec: Vec<Option<Agent>> = Vec::new();
+                for yy in 0..y {
+                    temp_vec.push(None);
+                }
+                f.push(temp_vec);
+            }
+
+            Field { field: f }
+        }
+
+        pub fn fill(&mut self, density: u32) {
+            let mut rng = thread_rng();
+
+            let max_x = self.field.len();
+            let max_y = self.field[0].len();
+
+            for x in 0..max_x {
+                for y in 0..max_y {
+                    if rng.next_u32() % density == 0 {
+                        self.field[x][y] = None;
+                    } else {
+                        self.field[x][y] = Some(Agent {
+                            colour: unsafe { transmute(rng.next_u32() as u8 % 2) },
+                            happiness: 0.0,
+                        });
+                    }
+                }
+            }
+        }
+
+        pub fn calculate_happiness(&mut self) {
+            let max_x = self.field.len();
+            let max_y = self.field[0].len();
+
+            for x in 0..max_x {
+                for y in 0..max_y {
+                    Self::get_and_set_happiness(self, x, y);
+                }
+            }
+        }
+
+        fn get_and_set_happiness(field: &mut Field, x: usize, y: usize) {
+            let max_x = field.field.len();
+            let max_y = field.field[0].len();
+
+            field.field[x][y] = {
+                let mut yellow = 0;
+                let mut green = 0;
+
+                for xx in -1..1 {
+                    for yy in -1..1 {
+                        let x_index = x + xx as usize % max_x;
+                        let y_index = y + yy as usize % max_y;
+
+                        if field.field[x_index][y_index] == None {
+                            continue;
+                        }
+                        if field.field[x_index][y_index].unwrap().colour == Colour::Yellow {
+                            yellow += 1;
+                        } else if field.field[x_index][y_index].unwrap().colour == Colour::Green {
+                            green += 1;
+                        }
+                    }
+                }
+
+                if field.field[x][y].unwrap().colour == Colour::Yellow {
+                    Some(Agent {
+                        colour: Colour::Yellow,
+                        happiness: yellow as f32 / (yellow + green) as f32,
+                    })
+                } else if field.field[x][y].unwrap().colour == Colour::Green {
+                    Some(Agent {
+                        colour: Colour::Green,
+                        happiness: green as f32 / (yellow + green) as f32,
+                    })
+                } else {
+                    None
+                }
+            };
+        }
+    }
+}
